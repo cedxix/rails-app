9d82b28f12b027d0aa79ca520d9584f2da76ef08
Cedric D. ANIDIE <cedric.anidie@gmail.com>
2019-10-23 08:03:53 +0200

    implementing concerns for Placeholder image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,17 @@
 class Portfolio < ApplicationRecord
-    validates_presence_of :title, :body, :main_image, :thumb_image
+  include Placeholder
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
-    def self.angular
-        where(subtitle: 'Angular')
-    end
+  def self.angular
+      where(subtitle: 'Angular')
+  end
 
-    scope :ruby_on_rails_portfolio_item, -> { where(subtitle: 'Ruby on Rails')}
+  scope :ruby_on_rails_portfolio_item, -> { where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(h:1250,w:600)
+    self.thumb_image ||= Placeholder.image_generator(h:350,w:200)
+  end
 end
